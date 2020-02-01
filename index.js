c6c470f91a89c34a6e3dbac704a504b76faa2539
Jeevitha <jeevithac1999@gmail.com>
2020-02-01 10:11:42 +0530

    simple node js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ http
 .createServer((req, res)=>{
     //res.write('hi');
     //res.writeHead(200,{"Content-Type":"text/html"});
-    res.writeHead(200,{"Content-Type":"application/json"});//Mime type
+    res.writeHead(200,{"Content-Type":"application/json"});//type is refered by a format called Mime type
     const queryObject=getQueryString(req.url);
     res.write(JSON.stringify(queryObject));
+    console.log("h")
     //res.write(JSON.stringify({status:"success"}));
     res.end();
 })
